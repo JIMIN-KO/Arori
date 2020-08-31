fe7b1ccea095b4410489880d3b980576b951bb29
jimin <jimin@192.168.200.177>
2020-08-31 17:43:25 +0900

    클래스 오류 수정

--- ARORI/src/main/java/com/kh/arori/repository/study/ClassesDao.java
+++ ARORI/src/main/java/com/kh/arori/repository/study/ClassesDao.java
@@ -1,18 +1,7 @@
 package com.kh.arori.repository.study;
 
 import com.kh.arori.entity.study.ClassesDto;
-
-public interface ClassesDao {
-	
-	// 클래스 시퀀스 발급
-	public int getSeq();
-	// 클래스 생성
-	public void createClasses(ClassesDto classesDto);
-}
-=======
 import java.util.List;
-
-import com.kh.arori.entity.study.ClassesDto;
 
 public interface ClassesDao {
 
--- ARORI/src/main/java/com/kh/arori/repository/study/ClassesDaoImpl.java
+++ ARORI/src/main/java/com/kh/arori/repository/study/ClassesDaoImpl.java
@@ -4,34 +4,7 @@ import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import com.kh.arori.entity.study.ClassesDto;
-
-@Repository
-public class ClassesDaoImpl implements ClassesDao {
-
-	@Autowired
-	private SqlSession sqlSessioin;
-	
-	// 시퀀스 생성
-	@Override
-	public int getSeq() {
-		int c_no = sqlSessioin.selectOne("classes.getSeq");
-		return c_no;
-	}
-
-	// 클래스 생성(DB에 삽입)
-	@Override
-	public void createClasses(ClassesDto classesDto) {
-		sqlSessioin.insert("classes.createClasses", classesDto);
-	}
-
-}
-=======
 import java.util.List;
-
-import org.apache.ibatis.session.SqlSession;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
 
 import com.kh.arori.entity.study.ClassesDto;
 
