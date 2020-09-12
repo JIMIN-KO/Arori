da23ab167167acafd667886874f25a158bb7cd86
d0 <dip.0cean@daum.net>
2020-09-12 23:52:36 +0900

    퀴즈 / 퀘스쳔 /정답 Update 밑 Delete 구문 완성

--- ARORI/src/main/java/com/kh/arori/repository/study/QuestionDao.java
+++ ARORI/src/main/java/com/kh/arori/repository/study/QuestionDao.java
@@ -14,42 +14,68 @@ public interface QuestionDao {
 	// 퀘스천 시퀀스
 	public int getSeq();
 
-	// ox답안 시퀀스
-	public int getSeqOx();
-
-	// 선다형 답안 시퀀스
-	public int getSeqMul();
-
-	// 단답,서술형 답안 시퀀스
-	public int getSeqEx();
-
 	// 퀘스천 생성
 	public void createQuestion(AllQuestionDto allQuestionDto);
 
 	// 퀘스쳔 수정 / 갱신
 	public int updateAQuestion(AllQuestionDto allQuestionDto);
 
+	//  퀘스쳔 삭제
+	public int deleteQuestion(AllQuestionDto allQuestionDto);
+
+	// 퀘스쳔 단일 조회
+	public AllQuestionDto get(AllQuestionDto allQuestionDto);
+
+	// 해당 퀴즈의 퀘스쳔 정보 불러오기
+	public List<AllQuestionDto> getQuestion(int q_no);
+
+	// 해당 퀴즈의 퀘스천 정보 + 정답 불러오기 (THIS_QUIZ)
+	public List<ThisQuizDto> getTQ(int q_no);
+
+	// 퀘스쳔 오작교
+	// 퀘스쳔 오작교 시퀀스 번호 발급
+	public int getSeq2(String table_name);
+
+	// 퀘스쳔 오작교 데이터 삽입
+	public void insertThis(This_qDto this_qDto);
+
+	// OX
+	// OX 답안 시퀀스
+	public int getSeqOx();
+
 	// OX 정답 생성
 	public void createOx(OxDto oxDto);
+
+	// OX 정답 수정
+	public int updateOx(OxDto oxDto);
+
+	// OX 정답 삭제
+	public int deleteOx(OxDto oxDto);
+
+	// 선다형
+	// 선다형 답안 시퀀스
+	public int getSeqMul();
 
 	// 선다형 정답 생성
 	public void createMul(MultipleDto multipleDto);
 
+	// 선다형 정답 수정
+	public int updateMul(MultipleDto multipleDto);
+
+	// 선다형 정답 삭제
+	public int deleteMul(MultipleDto multipleDto);
+
+	// 단답형
+	// 단답형 답안 시퀀스
+	public int getSeqEx();
+
 	// 단답형 정답 생성
 	public void createEx(ExplainDto explainDto);
-	
-	// 퀘스쳔 오작교 시퀀스 번호 발급 
-	public int getSeq2(String table_name);
-	
-	// 퀘스쳔 오작교 데이터 삽입
-	public void insertThis(This_qDto this_qDto);
-	
-	// 퀘스쳔 단일 조회 
-	public AllQuestionDto get(AllQuestionDto allQuestionDto);
-	
-	// 해당 퀴즈의 퀘스쳔 정보 불러오기 
-	public List<AllQuestionDto> getQuestion(int q_no);
-	
-	// 해당 퀴즈의 퀘스천 정보 + 정답 불러오기 (THIS_QUIZ)
-	public List<ThisQuizDto> getTQ(int q_no);
+
+	// 단답형 정답 수정
+	public int updateEx(ExplainDto explainDto);
+
+	// 단답형 정답 삭제
+	public int deleteEx(ExplainDto explainDto);
+
 }
